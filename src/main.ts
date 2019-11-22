81ef9d519ec659e72782290f401d72ef01ac1dd9
Doru Paraschiv <doru.paraschiv@vgw.co>
2019-11-22 12:59:49 +0200

    updated swagger definition description and tags

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const options = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Battletech mechs API')
+    .setDescription('The Battletech mechs API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('Battletech')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  
+
   await app.listen(3000);
 }
 bootstrap();
