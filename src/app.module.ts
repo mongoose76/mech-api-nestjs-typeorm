768f9dd841f64bf20b70e5775b73fa67f4a540ee
Doru Paraschiv <doru.paraschiv@vgw.co>
2019-11-22 11:46:28 +0200

    added typeorm to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BattleMech } from './battlemech/battlemech.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nestjs-jackpots',
+      entities: [BattleMech],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
