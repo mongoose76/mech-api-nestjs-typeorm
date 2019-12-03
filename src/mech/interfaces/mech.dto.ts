ff7b8fc8548c11f219ec8d5d9fd9fdfae4750543
Doru Paraschiv <doru.paraschiv@vgw.co>
2019-12-03 10:34:39 +0200

    use global validation pipe

--- src/mech/interfaces/mech.dto.ts
+++ src/mech/interfaces/mech.dto.ts
@@ -1,12 +1,17 @@
 import { MechHardpointDto } from './mechHardpoint.dto';
 import { MechClass } from './mechEnums';
+import { IsNotEmpty, IsInt } from 'class-validator';
 
 export class MechDto {
   readonly typeId: number;
   readonly subtype: string;
   readonly class: MechClass;
   readonly stockRole: string;
+
+  @IsNotEmpty()
+  @IsInt()
   readonly tonnageTotal: number;
+
   readonly tonnageFree: number;
   readonly damageMelee: number;
   readonly damageDFA: number;
